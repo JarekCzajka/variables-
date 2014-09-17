c255d36934bd79d08745fbe2983a5af417baefee
Jaroslaw Czajka <40681@longroad.ac.uk>
2014-09-17 15:21:02 +0100

    grass exercise

---
+++ Grass exercise.py
@@ -0,0 +1,24 @@
+#Jaroslaw Czajka
+#17.09.2014
+#Strech and Challenge Exercises
+
+#This program will ask the user about the measurments of his/her garden.
+print("Hello,today I am going to ask you for the mesurements of your garden")
+length=int(input("Please enter the length of your garden:"))
+width=int(input("PLease enter the width of your garden:"))
+#The program asked the user for the measurements of his/her garden.
+area=input(length*width)
+print("This is the area of your garden")
+#Now the program is asking the user about the perimeter of the garden
+int(input("Please enter the length of your garden:"))
+int(input("Please enter the width of your garden:"))
+perimeter=input(length*2+width*2)
+
+            
+
+                        
+
+
+
+
+         
---
+++ python shell grass exercise.py
@@ -0,0 +1,457 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 10, in <module>
+    area=int(input(length*width))
+ValueError: invalid literal for int() with base 10: ''
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:1
+10
+This is the area of your gardne
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your gardne
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your gardne:area
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:1
+10
+This is the area of your gardne
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Now,Please enter the perimeter of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Now,Please enter the perimeter of your garden:
+Now,Please enter the perimeter of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Now,Please enter the perimeter of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Now,Please enter the perimeter of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:1
+10
+This is the area of your garden
+The perimter of the garden is length*2+width*2
+Now,Please enter the perimeter of your garden:10
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+The perimter of the garden is length*2+width*2
+Please enter the lenght of your garden:{0}
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+The perimter of the garden is length*2+width*2
+Please enter the lenght of your garden:10
+Now,Please enter the perimeter of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 12, in <module>
+    print("{0}+{1}={2}".format(lenght,width,area))
+NameError: name 'lenght' is not defined
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+The perimter of the garden is length*2+width*2
+Please enter the length of your garden:40
+Please enter the width of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:Please enter the length of your garden:10
+The perimter of the garden is length*2+width*2
+Please enter the length of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+The perimter of the garden is length*2+width*2
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+Now,Please enter the perimeter of your garden:10
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+The perimter of the garden is length*2+width*2
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+Now,Please enter the perimeter of your garden:40
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+The perimter of the garden is length*2+width*2
+Please enter the length of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:109
+1090
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the length of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:01
+10
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+Please enter the width of your garden:
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+Now,Please enter the perimeter of your garden:40
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 15, in <module>
+    perimeter=input(length*2,width*2)
+TypeError: input expected at most 1 arguments, got 2
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 15, in <module>
+    perimeter=input(length,width)
+TypeError: input expected at most 1 arguments, got 2
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+100
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+20
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+20
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+10010
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+20
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+20
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+20
+perimeter
+<built-in method format of str object at 0x001B8700>
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+Traceback (most recent call last):
+  File "U:/git/variables-/grass.py", line 15, in <module>
+    perimeter=input(length+width,length+width)
+TypeError: input expected at most 1 arguments, got 2
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+20
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+20
+>>> ================================ RESTART ================================
+>>> 
+Hello,today I am going to ask you for the mesurements of your garden
+Please enter the length of your garden:10
+PLease enter the width of your garden:10
+100
+This is the area of your garden
+Please enter the length of your garden:10
+Please enter the width of your garden:10
+40
