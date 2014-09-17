88d76794d74af52ac8ef2a54de417b9e498644ce
Jaroslaw Czajka <40681@longroad.ac.uk>
2014-09-17 15:19:28 +0100

    completed

---
+++ Hello_world.py program.py
@@ -0,0 +1,11 @@
+#Jaroslaw Czajka
+# 08_09_2014
+#Exercise 1.1_Hello World
+
+print("hello world")
+#get the name of the user and store it into a variable
+first_name=input("Please enter your name:")
+#print out the name stored in the variable first_name
+print(first_name)
+#print out the name as part of the message
+print ("Hello {0}!".format(first_name))
---
+++ Python exercise thing 1.1.py
@@ -0,0 +1,9 @@
+#Jaroslaw Czajka
+#15.09.2014
+#Exrecise Thing 1.1
+
+print("Please think of a number")
+print("Please think of a different number")
+number=int(input("Please enter the number:"))
+number2=int(input("Please enter the differnet number:"))
+
