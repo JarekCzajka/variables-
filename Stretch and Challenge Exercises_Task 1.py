bf3bec0408b00abcda1b8685f09069684c031293
Jaroslaw Czajka <40681@longroad.ac.uk>
2014-09-22 10:24:40 +0100

    Stretch and Challenge

---
+++ Stretch and Challenge Exercises_Task 1.py
@@ -0,0 +1,13 @@
+#Jaroslaw Czajka
+#22.09.2014
+#Stretch and Challenge Exercises_Task 1
+
+#This program will ask the user for the measurements of users garden and calculate the perimeter and area of the gardne
+print("Welcome to GardenUnit")
+print("This program will help you to find the area and the perimeter of your garden")
+number_one=int(input("Please enter the length of your garden:"))
+#The program has asked the user for the length of the garden and dipslyed the result
+print("This is the length of your garden:")
+print(number_one)
+#The program has displyed the result
+int(input("Now please enter the width of your garden:"))
